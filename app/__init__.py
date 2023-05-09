9d769564cbec9819edc1685a348c6a8cd2c8e9cd
Abe Stokes <stokes.abraham12345@gmail.com>
2023-05-09 10:31:40 -0400

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from  flask import Flask
+from config import Config
+from .site.routes import site
+from .authentication.routes import auth
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db as root_db, login_manager, ma
+from .api.routes import api
+from flask_cors import CORS
+from helpers import JSONEncoder
+
+
+
+
+
+# create instance of flask in the app varible
+app = Flask(__name__)
+CORS(app)
+
+# Put all the html sites connected to app Flask object 
+# With the site blueprint object - which has HTML stuff and routes
+app.register_blueprint(site)
+app.register_blueprint(auth)
+app.register_blueprint(api)
+
+app.json_encoder = JSONEncoder()
+app.config.from_object(Config)
+
+
+# initiates the database app (db is a SQLAlchemy object)
+root_db.init_app(app)
+login_manager.init_app(app)
+
+#links the marshmallow object to the app- 
+# ma in charge of connecting and adding stuff to db 
+ma.init_app(app)
+migrate = Migrate(app, root_db)
+   
+
+
---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, 'env'))
+
+class Config():
+    '''
+        Set the config variables for the flask app
+        using Enviroment variables where available .
+        OTherwise, create the config variables if not done already.
+    '''
+
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Raghav will never get acces to my CSS'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
